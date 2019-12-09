9fa78dfec3cdd224ba8121ae10668176107a2f4a
Kevin Yeh <kevin@shopvida.com>
2019-12-08 21:06:52 -0800

    created app with redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+import App from './app/App'
 import * as serviceWorker from './serviceWorker'
 import { BrowserRouter } from 'react-router-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { createLogger } from 'redux-logger'
+import thunk from 'redux-thunk'
+import promise from 'redux-promise'
+
+import reducer from './reducer.js'
+
+const logger = createLogger()
+
+const store = createStore(reducer, applyMiddleware(thunk, promise, logger))
 
 ReactDOM.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>,
+	<Provider store={store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>,
 	document.getElementById('root')
 )
 
--- src/projectInfo/ProjectInfo.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const ProjectInfo = props => <p>hi</p>
-
-export default ProjectInfo
